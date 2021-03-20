8d5cd3aafbcd761abb19e056a01f4c850f9973c9
heqinghuang <hhuang@goodwatercap.com>
2021-03-20 10:09:42 -0700

    Remove some random comments

--- main.py
+++ main.py
@@ -1,17 +1,8 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 import grpc
 
 # import the generated classes
 import protos.bank_system_pb2
 import protos.bank_system_pb2_grpc
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
@@ -31,5 +22,3 @@ if __name__ == '__main__':
 
     print(processEvent)
     print(syncBranch)
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
