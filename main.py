e7e477f8d1d038130f5659f1ff16a89021fee0c6
heqinghuang <huangheqing426@gmail.com>
2021-03-20 20:11:33 -0700

    Validate the python script arguments before processing requests

--- main.py
+++ main.py
@@ -21,38 +21,38 @@ class Server(protos.bank_system_pb2_grpc.BankSystemServicer):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    filename = sys.argv[1]
-    try:
-        with open(sys.argv[1]) as f:
-            input_data = json.load(f)
-            print(input_data[1])
-    except "Not able to process file":
-        print(f'not able to load the json file: {filename}')
+    if len(sys.argv) == 2 and sys.argv[0] == 'main.py':
+        filename = sys.argv[1]
+        try:
+            with open(sys.argv[1]) as f:
+                input_data = json.load(f)
+        except "Not able to process file":
+            print(f'not able to load the json file: {filename}')
 
-    if len(input_data) > 1:
-        for data in input_data:
-            print(data)
+        if len(input_data) > 1:
+            for data in input_data:
+                print(data)
 
-    # open a gRPC channel
-    channel = grpc.insecure_channel('localhost:8081')
+        # open a gRPC channel
+        channel = grpc.insecure_channel('localhost:8081')
 
-    # create a stub (client)
-    stub = protos.bank_system_pb2_grpc.BankSystemStub(channel)
+        # create a stub (client)
+        stub = protos.bank_system_pb2_grpc.BankSystemStub(channel)
 
-    # Generate x amount of services based on the input files branches/custoemrs
-    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-    protos.bank_system_pb2_grpc.add_BankSystemServicer_to_server(Server(), server)
-    print('Starting server. Listening on port 8081.')
-    server.add_insecure_port('localhost:8081')
-    server.start()
+        # Generate x amount of services based on the input files branches/custoemrs
+        server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+        protos.bank_system_pb2_grpc.add_BankSystemServicer_to_server(Server(), server)
+        print('Starting server. Listening on port 8081.')
+        server.add_insecure_port('localhost:8081')
+        server.start()
 
-    # create a valid request message
-    customer = protos.bank_system_pb2.Customer(id=1, type="", events=[""])
-    branch = protos.bank_system_pb2.Branch(id=1, type="", balance=200)
+        # create a valid request message
+        customer = protos.bank_system_pb2.Customer(id=1, type="", events=[""])
+        branch = protos.bank_system_pb2.Branch(id=1, type="", balance=200)
 
-    # make the call
-    processEvent = stub.processEvent(customer)
-    syncBranch = stub.syncBranch(branch)
+        # make the call
+        processEvent = stub.processEvent(customer)
+        syncBranch = stub.syncBranch(branch)
 
-    # print(processEvent)
-    # print(syncBranch)
+        # print(processEvent)
+        # print(syncBranch)
